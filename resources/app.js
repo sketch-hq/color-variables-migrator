9557aa60697cc6733d67af1a38d8ccf1fb0104b3
Ale M <bomberstudios@gmail.com>
2020-09-21 17:14:01 +0200

    plugin working with 3 options

---
+++ resources/app.js
@@ -0,0 +1,34 @@
+// Disable the context menu to have a more native feel
+//document.addEventListener("contextmenu", e => e.preventDefault())
+let allInputs = [];
+[
+  'option-1',
+  'option-2',
+  'option-3'
+].forEach(id => {
+  allInputs.push(document.getElementById(id))
+})
+
+const getOptions = () => {
+  let obj = {}
+  allInputs.forEach(input => {
+    let val = Number(input.value)
+
+    if (input.type === 'checkbox') {
+      val = Boolean(input.checked)
+    } else if (input.type === 'text') {
+      val = input.value
+    }
+
+    obj[input.id] = val
+  })
+
+  return obj
+}
+
+document.getElementById('cancel').addEventListener('click', function() {
+  window.postMessage('cancel')
+})
+document.getElementById('submit').addEventListener('click', function() {
+  window.postMessage('performMigration', getOptions())
+})
--- src/my-command.js
+++ src/my-command.js
@@ -1,48 +1,105 @@
-const sketch = require('sketch')
+import sketch from 'sketch'
+import BrowserWindow from 'sketch-module-web-view'
+import { getWebview } from 'sketch-module-web-view/remote'
+import UI from 'sketch/ui'
+
 const Swatch = sketch.Swatch
 const doc = sketch.getSelectedDocument()
+const webviewIdentifier = 'color-variables-migrator.webview'
 
-export function migrateColors(context){
-  // When you open an existing document in Sketch 69, the color assets in the document will be migrated to Color Swatches. However, layers using those colors will not be changed to use the new swatches. This plugin takes care of this
-  // But first, ask the user for confirmation!
-  // TODO: make a proper confirmation dialog for this thing,
-  // and maybe add it to the API
-  sketch.UI.getInputFromUser(
-    "Replace existing colors with Color Variables?",
-    {
-      initialValue: 'Yes',
-    },
-    (err, value) => {
-      if (err) {
-        // most likely the user canceled the input
-        return
-      } else {
-        const doc = sketch.getSelectedDocument()
-        let swatches = doc.swatches
-        if (swatches.length == 0) {
-          // Nothing to see here, move along
-          return
-        }
+export function migrate(context) {
+  const options = {
+    identifier: webviewIdentifier,
+    width: 400,
+    height: 430,
+    show: false,
+    resizable: false,
+    title: 'Color Variables Migrator',
+    minimizable: false,
+    maximizable: false,
+    backgroundColor: '#ececec',
+    hidesOnDeactivate: false
+  }
 
-        const allLayers = sketch.find('*') // we can use something else if we don't want to bring Sketch down... Try with larger sample docs and see what happens...
-        allLayers.forEach(layer => {
-          layer.style.fills.filter(fill => fill.fillType == 'Color').forEach(fill => {
-            const layerColor = fill.color
-            let swatch = matchingSwatchForColor(layerColor)
-            if (!swatch) {
-              return
-            }
-            let newColor = swatch.referencingColor
-            fill.color = newColor
-          })
-          // TODO: do the same with borders!
-        })
-      }
-    }
-  )
+  const browserWindow = new BrowserWindow(options)
+
+  browserWindow.once('ready-to-show', () => {
+    browserWindow.show()
+  })
+
+  const webContents = browserWindow.webContents
+
+  webContents.on('performMigration', options => {
+    browserWindow.close()
+    performMigration(options)
+  })
+  webContents.on('cancel', () => {
+    browserWindow.close()
+    UI.message('Migration cancelled. Nothing was changed in your document.')
+  })
+
+  browserWindow.loadURL(require('../resources/dialog.html'))
 }
 
-export function migrateStyles(context) {
+// When the plugin is shutdown by Sketch (for example when the user disable the plugin)
+// we need to close the webview if it's open
+export function onShutdown() {
+  const existingWebview = getWebview(webviewIdentifier)
+  if (existingWebview) {
+    existingWebview.close()
+  }
+}
+
+function performMigration(options) {
+  const replaceColors = options['option-1']
+  const generateColors = options['option-2']
+  const simplifyStyles = options['option-3']
+  if (!replaceColors && !generateColors && !simplifyStyles) {
+    // Yo, you know there's a Cancel button for this, right?
+    cancel()
+  }
+  // We'll first perform this operation, because otherwise the other two may produce unexpected results
+  if (simplifyStyles) {
+    doSimplifyStyles()
+  }
+  // Then we want to make sure all our layers are using Color Variables, and not regular colors...
+  if (replaceColors) {
+    doMigrateColors()
+  }
+  // ...and only then are we ready to generate new Color Variables from Layer and Text Styles
+  if (generateColors) {
+    doMigrateStyles()
+  }
+  UI.message('Color migration complete.')
+}
+
+function doMigrateColors(context){
+  // When you open an existing document in Sketch 69, the color assets in the document will be migrated to Color Swatches. However, layers using those colors will not be changed to use the new swatches. This plugin takes care of this
+  const allLayers = sketch.find('*') // TODO: optimise this query
+  allLayers.forEach(layer => {
+    layer.style.fills.filter(fill => fill.fillType == 'Color').forEach(fill => {
+      const layerColor = fill.color
+      let swatch = matchingSwatchForColor(layerColor)
+      if (!swatch) {
+        return
+      }
+      let newColor = swatch.referencingColor
+      fill.color = newColor
+    })
+    layer.style.borders.filter(border => border.fillType == 'Color').forEach(border => {
+      const layerColor = border.color
+      let swatch = matchingSwatchForColor(layerColor)
+      if (!swatch) {
+        return
+      }
+      let newColor = swatch.referencingColor
+      border.color = newColor
+    })
+  })
+}
+
+function doMigrateStyles(context) {
+  // This method traverses all Layer and Text Styles, and makes sure they use Color Swatches that exist in the document.
   // list all layer styles in document
   const allLayerStyles = doc.sharedLayerStyles
   allLayerStyles.forEach( style => {
@@ -68,7 +125,10 @@ export function migrateStyles(context) {
   })
 }
 
-export function convertStyles(context) {
+function doSimplifyStyles(context) {
+  // Prior to Sketch 69's Color Variables, it was common to use Layer and Text Styles with a single color to emulate color tokens. Updating the style would then change all the layer using that style.
+  // In Sketch 69, this hack is no longer necessary. This method will convert all Layer and Text Styles that consist of only 1 color to an equivalent Color Variable, assign that Color Variable to all layers that used the original Layer or Text Style, and finally remove the Style.
+
   // Thinking about this, I guess it makes sense to merge this and migrateStyles
   // I'll keep them separated for the time being, but there will be a lot of code duplication
 
@@ -81,7 +141,6 @@ export function convertStyles(context) {
   const stylesToRemove = []
   allLayerStyles.forEach( style => {
     let currentStyle = style.style
-
     // TODO: I should really be checking for enabled fills here, but this will do by now
     if (currentStyle.fills.length == 1) {
       const swatchName = 'Migrated Styles/' + style.name + '-fill'
@@ -118,7 +177,7 @@ export function convertStyles(context) {
         styleInstance.syncWithSharedStyle(style)
       })
       stylesToRemove.push(style)
-     }
+    }
   })
   stylesToRemove.forEach( style => {
     doc.sharedLayerStyles = doc.sharedLayerStyles.filter( item => item.id != style.id )
